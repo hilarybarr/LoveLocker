d4fed502be6567e0cde731c72db092d6fdd0ebf0
hilarybarr <hilaryrosebarr@gmail.com>
2014-07-29 20:12:27 -0400

    fix migration

--- db/migrate/20130501155942_create_notes.rb
+++ db/migrate/20130501155942_create_notes.rb
@@ -1,7 +1,7 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
-      t.string :content
+      t.text :content
       t.string :toward
       t.string :to
       t.references :lover
