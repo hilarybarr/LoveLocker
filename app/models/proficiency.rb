35f1b2f242749184279922f9cc209a37fbfb9a76
hilarybarr <hilaryrosebarr@gmail.com>
2014-07-29 20:53:36 -0400

    deleted unnecessary

--- app/models/proficiency.rb
+++
@@ -1,8 +0,0 @@
-class Proficiency < ActiveRecord::Base
- validates :years_experience, :presence => true
- validates :formal?, :inclusion => {:in => [true, false]}
- 
-  belongs_to :user
-  belongs_to :skill
- 
-end
